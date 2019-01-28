3aeba71bc0d10159e5d9e438ca29ab8094df2b38
jecaro <jeancharles.quillet@gmail.com>
2019-01-28 19:36:37 +0100

    Added synopsis and basic tables

--- Main.hs
+++ Main.hs
@@ -19,8 +19,33 @@ import HalfDayType
 import TimeInDay
 import Office
 
+-- Synopsis
+-- hsmaster [date] [morning|afternoon] [modifiers]
+--   default date: today
+--   no TimeInDay: both morning and afernoon
+--   no modifiers: print the records
+--   modifiers: 
+--     -p project   set project by name works without TimeInDay
+--     -n note      set note
+--     -an note     append note
+--     -a 00:00     set arrived time
+--     -l 00:00     set left time
+--     -d           delete record work without TimeInDay
+--     -edit        launch editor
+--  date specifier: 
+--      today
+--      tomorrow
+--      yesterday   
+--      12/02       Date on current year
+--      12/02/19    Date on specific year
+
+-- TODO:
+-- - Add consistency check
+-- - Add import CSV
+-- - Add stats for a year
+
 -- Ideas
--- - put default values for starting ending day in a config file
+-- - put default values for starting ending time in a config file
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
@@ -46,7 +71,7 @@ HalfDayWorked -- Only for WorkedOpenDay
 	arrived   TimeOfDay 
 	left      TimeOfDay --Constraint Left > Arrived
     office    Office
-    projectId ProjectId
+    projectId ProjectId 
     halfDayId HalfDayId
     deriving Show
 |]
@@ -94,3 +119,4 @@ main = do
         -- Delete 
         delete halfDayId
 
+        
