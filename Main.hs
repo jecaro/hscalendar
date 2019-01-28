3ca3ebf4d369238c0e29d74b50c7e6958992db88
jecaro <jeancharles.quillet@gmail.com>
2019-01-28 19:40:16 +0100

    Removed tables from example

--- Main.hs
+++ Main.hs
@@ -46,25 +46,18 @@ import Office
 
 -- Ideas
 -- - put default values for starting ending time in a config file
+-- - put file db in a config file as well
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person
+Project
     name String
-    age  Int Maybe
-    deriving Show
-BlogPost
-    title    String
-    authorId PersonId
+    UniqueName name
     deriving Show
 HalfDay
     day         Day        
     timeInDay   TimeInDay   -- morning/afternoon
     halfDayType HalfDayType -- worked/holiday
     FullDay day timeInDay   -- One morning, one afternoon everyday
-    deriving Show
-Project
-    name String
-    UniqueName name
     deriving Show
 HalfDayWorked -- Only for WorkedOpenDay
 	notes     String
@@ -83,21 +76,6 @@ main = do
 
     runSqlite ":memory:" $ do
         runMigration migrateAll
-
-        johnId <- insert $ Person "John Doe" $ Just 35
-        janeId <- insert $ Person "Jane Doe" Nothing
-
-        insert $ BlogPost "My fr1st p0st" johnId
-        insert $ BlogPost "One more for good measure" johnId
-
-        oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
-        liftIO $ print (oneJohnPost :: [Entity BlogPost])
-
-        john <- get johnId
-        liftIO $ print (john :: Maybe Person)
-
-        delete janeId
-        deleteWhere [BlogPostAuthorId ==. johnId]
 
         -- New project and half day
         projectId <- insert $ Project "A project"
@@ -119,4 +97,3 @@ main = do
         -- Delete 
         delete halfDayId
 
-        
