2c52a9813aad998fc4ad712605769bc4fceafebc
jecaro <jeancharles.quillet@gmail.com>
2019-03-05 18:15:27 +0100

    Implement hdGet hdwGet et hdAndHdwGet

--- ModelFcts.hs
+++ ModelFcts.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE FlexibleInstances           #-}
 
 module ModelFcts 
-  ( projAdd
+  ( ModelException(..)
+  , projAdd
   , projExists
   , projGet
   , projList
   , projRm
-  , ModelException(..) 
+  , hdGet
+  , hdwGet
   ) where
 
 import           Control.Exception.Safe 
   ( Exception
+  , MonadCatch
   , MonadThrow
   , throwM
+  , try
   )
 import           Control.Monad.IO.Class (MonadIO)
 import           Database.Persist.Sqlite 
@@ -27,9 +31,12 @@ import           Database.Persist.Sqlite
    , (==.)
    )
 
-import           Data.Maybe (isJust)
+import           Data.Maybe (isJust, isNothing)
+import           Data.Time.Calendar (Day)
 
-import Model
+import           Model
+import           TimeInDay(TimeInDay)
+import           HalfDayType(HalfDayType(..))
 
 newtype ModelException = ModelException String deriving (Show)
 
@@ -40,6 +47,15 @@ errProjNotFound name = "The project " ++ name ++ " is not in the database"
 
 errProjExists :: String -> String
 errProjExists name = "The project " ++ name ++ " exists in the database"
+
+errHdNotFound :: Day -> TimeInDay -> String
+errHdNotFound day tid = "Nothing for " ++ show day ++ " " ++ show tid
+
+errHdwNotFound :: HalfDayId -> String
+errHdwNotFound hdId = "No half-day worked entry for " ++ show hdId
+
+errDbInconsistency :: String
+errDbInconsistency = "Warning db inconsistency"
 
 projGet :: (MonadIO m, MonadThrow m) => String -> SqlPersistT m (Entity Project)
 projGet name = do
@@ -68,3 +84,35 @@ projRm name = do
   (Entity pId _) <- projGet name 
   deleteWhere [HalfDayWorkedProjectId ==. pId]
   delete pId
+
+hdGet :: (MonadIO m, MonadThrow m) => Day -> TimeInDay -> SqlPersistT m (Entity HalfDay)
+hdGet day tid = do
+  mbHd <- getBy $ DayAndTimeInDay day tid
+  case mbHd of
+    Nothing  -> throwM $ ModelException $ errHdNotFound day tid
+    Just e   -> return e
+
+-- todo unwrap entity
+hdwGet :: (MonadIO m, MonadThrow m) => (Entity HalfDay) -> SqlPersistT m (Entity HalfDayWorked)
+hdwGet (Entity hdId _) = do
+  mbHdw <- getBy $ UniqueHalfDayId hdId
+  case mbHdw of
+    Nothing  -> throwM $ ModelException $ errHdwNotFound hdId
+    Just e   -> return e
+
+hdAndHdwGet 
+  :: (MonadIO m, MonadCatch m) 
+  => Day
+  -> TimeInDay 
+  -> SqlPersistT m (Entity HalfDay, Maybe (Entity HalfDayWorked))
+hdAndHdwGet day tid = do
+  hd@(Entity _ eHd) <- hdGet day tid 
+  eiHdw <- try $ hdwGet hd
+  let mbHdw = case eiHdw of 
+                Left (ModelException _) -> Nothing
+                Right e                 -> Just e
+  -- Check for consistency
+  case (eHd, mbHdw) of
+    (HalfDay _ _ Worked, Nothing) -> throwM $ ModelException errDbInconsistency
+    (HalfDay _ _ Holiday, Just _) -> throwM $ ModelException errDbInconsistency
+    (_, _) -> return (hd, mbHdw)
