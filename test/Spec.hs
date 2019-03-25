b13aba1993d6d3eb59f9eea9ca7df18538e12db2
jecaro <jeancharles.quillet@gmail.com>
2019-03-25 18:18:51 +0100

    Add hspec example

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,17 @@
 import           RIO
+import qualified RIO.List.Partial as L (head) 
 
-import           Data.Text.IO (putStrLn)
+import           Test.Hspec
+import           Test.QuickCheck
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ 
+  describe "RIO.List.Partial.head" $ do
+    it "returns the first element of a list" $ 
+      L.head [23 ..] `shouldBe` (23 :: Int)
+
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> L.head (x:xs) == (x :: Int)
+
+    it "throws an exception if used with an empty list" $
+      evaluate (L.head []) `shouldThrow` anyException
