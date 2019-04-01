7d317c484491e79143de4ef68bb2f3518ddf488c
jecaro <jeancharles.quillet@gmail.com>
2019-04-01 16:29:39 +0200

    Add another way to test quickcheck properties

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,12 +4,15 @@ import           Database.Persist.Sqlite
     ( deleteWhere
     , Filter
     , runMigration
-    , runSqlConn
+    , runSqlPersistM
     , SqlBackend
+    , SqlPersistT
     , withSqliteConn
+    , runSqlPersistM
+    , SqlPersistM
     )
 import           Control.Monad.Logger (runNoLoggingT)
-import           Test.Hspec 
+import           Test.Hspec
     ( after_
     , before_
     , beforeAll
@@ -23,16 +26,18 @@ import           Test.Hspec
     , shouldThrow
     , Spec
     )
-import           Test.QuickCheck 
+import           Test.QuickCheck
     ( property
     , Property
+    , arbitrary
+    , ioProperty
     )
-import qualified Test.QuickCheck.Monadic as Q (assert, monadicIO, run)
+import qualified Test.QuickCheck.Monadic as Q (assert, pick, monadic, monadicIO, run, PropertyM(..))
 import           Test.QuickCheck.Instances.Text()
 
 import           Model
 import           ModelFcts
-    ( ModelException()
+    ( ModelException(..)
     , projAdd
     , projExists
     , projList
@@ -40,50 +45,67 @@ import           ModelFcts
     , projRm
     )
 
-runDB :: MonadUnliftIO m => SqlBackend -> ReaderT SqlBackend m a -> m a
-runDB conn actions = runSqlConn actions conn
+runDB :: SqlBackend -> SqlPersistM a -> IO a
+runDB =  flip runSqlPersistM 
 
-migrate :: MonadUnliftIO m => SqlBackend -> m ()  
+migrate :: SqlBackend -> IO ()
 migrate conn = runDB conn $ runMigration migrateAll
 
-cleanProjects :: MonadUnliftIO m => SqlBackend -> m ()
+cleanProjects :: SqlBackend -> IO ()
 cleanProjects conn = runDB conn $ deleteWhere ([] :: [Filter Project])
 
 modelException :: Selector ModelException
 modelException = const True
 
-projAddprojExists :: SqlBackend -> Project -> Property
-projAddprojExists conn project = Q.monadicIO $ do 
+projAddProjExists :: SqlBackend -> Project -> Property
+projAddProjExists conn project = Q.monadicIO $ do
     exists <- Q.run $ runDB conn (projAdd project >> projExists project)
     Q.assert exists
     noExists <- Q.run $ runDB conn (projRm project >> projExists project)
     Q.assert (not noExists)
 
+projAddProjAdd :: SqlBackend -> Project -> Property
+projAddProjAdd conn project = Q.monadicIO $ do
+    exceptionRaised <- Q.run $ runDB conn $ do
+        projAdd project
+        catch (projAdd project >> return False) (\(ModelException _) -> return True)
+    Q.assert exceptionRaised
+    Q.run $ runDB conn $ projRm project
+
+prop_projAdd :: (MonadIO m) => Q.PropertyM (SqlPersistT m) ()
+prop_projAdd = do
+    projName <- Q.pick arbitrary
+    let project = Project projName
+    Q.run $ projAdd project
+    exists <- Q.run $ projExists project
+    Q.assert exists
+    Q.run $ projRm project
+
 testProjAPI :: SqlBackend -> Spec
-testProjAPI conn = 
+testProjAPI conn =
     describe "Test the project API" $ do
         context "When the DB is empty" $ do
-            it "tests the uniqueness of the name" $ 
+            it "tests the uniqueness of the name" $
                 runDB conn (projAdd project1 >> projAdd project1)
                   `shouldThrow` modelException
-            it "tests if a project does not exists" $ 
+            it "tests if a project does not exists" $
                 runDB conn (projExists project1) `shouldReturn` False
-            it "tests if we can remove a project" $ 
+            it "tests if we can remove a project" $
                 runDB conn (projRm project1) `shouldThrow` modelException
-            it "tests if we can rename a project not present in the db" $ 
-                runDB conn (projRename project1 project2) 
+            it "tests if we can rename a project not present in the db" $
+                runDB conn (projRename project1 project2)
                     `shouldThrow` modelException
-            it "tests the list of projects" $ 
+            it "tests the list of projects" $
                 runDB conn projList `shouldReturn` []
-        context "One project in DB" 
+        context "One project in DB"
             $ before_ (runDB conn (projAdd project1))
             $ after_ (cleanProjects conn)
             $ do
-            it "tests if the project exists" $ 
+            it "tests if the project exists" $
                 runDB conn (projExists project1) `shouldReturn` True
             it "tests if we can remove the project" $  do
                 exists <- runDB conn $ do
-                    projRm project1 
+                    projRm project1
                     projExists project1
                 exists `shouldBe` False
             it "tests if we can rename it" $ do
@@ -92,16 +114,20 @@ testProjAPI conn =
                 proj1Exists `shouldBe` False
                 proj2Exists <- runDB conn (projExists project2)
                 proj2Exists `shouldBe` True
-            it "tests the list of projects" $ 
+            it "tests the list of projects" $
                 runDB conn projList `shouldReturn` [project1]
-        context "Test properties" $ 
+        context "Test properties" $ do
             it "projAdd projExists" $
-                property (projAddprojExists conn)
+                property (projAddProjExists conn)
+            it "projAdd projAdd" $
+                property (projAddProjAdd conn)
+            it "other prop" $
+                property (Q.monadic (ioProperty . runDB conn) prop_projAdd)
   where project1 = Project "TestProject1"
         project2 = Project "TestProject2"
 
 main :: IO ()
-main = runNoLoggingT . withSqliteConn ":memory:" $ \conn -> 
+main = runNoLoggingT . withSqliteConn ":memory:" $ \conn -> liftIO $ do
     liftIO $ hspec $ beforeAll (migrate conn) $ 
         testProjAPI conn
-        
+
