f06f9ea72d13c0d5f760ae49241fdd7e7851702e
jecaro <jeancharles.quillet@gmail.com>
2020-02-01 14:51:59 +0100

    Setup elm app
    - initialise model with current time
    - add bulma css

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,6 +1,40 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html,  div, text)
+import Html.Attributes exposing (class)
 
+
+type alias Msg = ()
+
+
+type alias Model = Int
+
+
+main : Program Int Model Msg
 main =
-    text "Hello, World!"
+    Browser.element 
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view 
+        }
+
+
+init : Int -> (Model, Cmd Msg)
+init i = (i, Cmd.none)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update _ model =
+    (model, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+view : Int -> Html Msg
+view i =
+    div [ class "container" ]
+        [ text ("Current Posix time is " ++ String.fromInt i)
+        ]
