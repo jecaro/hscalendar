ae8635945af61d32f1ac67685c10fbf0c7b8ab40
jecaro <jeancharles.quillet@gmail.com>
2020-02-02 13:43:20 +0100

    Change Posix instead of Int

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,40 +1,199 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html,  div, text)
+import Html exposing (Html, button, div, h1, nav, p, section, text)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import Platform.Cmd exposing (batch)
+import String exposing (padLeft)
+import Task exposing (perform)
+import Time
+    exposing
+        ( Month(..)
+        , Posix
+        , Weekday(..)
+        , Zone
+        , here
+        , millisToPosix
+        , now
+        , toDay
+        , toHour
+        , toMinute
+        , toMonth
+        , toSecond
+        , toYear
+        , utc
+        )
+import Time.Extra exposing (Interval(..), add)
 
 
-type alias Msg = ()
+type alias Model =
+    { zone : Zone
+    , posix : Posix
+    }
 
 
-type alias Model = Int
+type Msg
+    = AdjustZone Zone
+    | AdjustPosix Posix
 
 
-main : Program Int Model Msg
+main : Program () Model Msg
 main =
-    Browser.element 
+    Browser.element
         { init = init
         , subscriptions = subscriptions
         , update = update
-        , view = view 
+        , view = view
         }
 
 
-init : Int -> (Model, Cmd Msg)
-init i = (i, Cmd.none)
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( Model utc (millisToPosix 0)
+    , batch
+        [ perform AdjustZone here
+        , perform AdjustPosix now
+        ]
+    )
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update _ model =
-    (model, Cmd.none)
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AdjustZone zone ->
+            ( { model | zone = zone }, Cmd.none )
+
+        AdjustPosix posix ->
+            ( { model | posix = posix }, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
-view : Int -> Html Msg
-view i =
-    div [ class "container" ]
-        [ text ("Current Posix time is " ++ String.fromInt i)
+
+monthToInt : Month -> Int
+monthToInt m =
+    case m of
+        Jan ->
+            1
+
+        Feb ->
+            2
+
+        Mar ->
+            3
+
+        Apr ->
+            4
+
+        May ->
+            5
+
+        Jun ->
+            6
+
+        Jul ->
+            7
+
+        Aug ->
+            8
+
+        Sep ->
+            9
+
+        Oct ->
+            10
+
+        Nov ->
+            11
+
+        Dec ->
+            12
+
+
+posixToString : Zone -> Posix -> String
+posixToString z t =
+    let
+        year =
+            String.fromInt (toYear z t)
+
+        month =
+            padLeft 2 '0' (String.fromInt (monthToInt (toMonth z t)))
+
+        day =
+            padLeft 2 '0' (String.fromInt (toDay z t))
+
+        hour =
+            padLeft 2 '0' (String.fromInt (toHour z t))
+
+        minute =
+            padLeft 2 '0' (String.fromInt (toMinute z t))
+
+        second =
+            padLeft 2 '0' (String.fromInt (toSecond z t))
+    in
+    day ++ "/" ++ month ++ "/" ++ year ++ "-" ++ hour ++ ":" ++ minute ++ ":" ++ second
+
+
+previousDay : Model -> Posix
+previousDay model =
+    add Day -1 model.zone model.posix
+
+
+nextDay : Model -> Posix
+nextDay model =
+    add Day 1 model.zone model.posix
+
+
+
+--<section class="hero">
+--  <div class="hero-body">
+--    <div class="container">
+--      <h1 class="title">
+--        Hero title
+--      </h1>
+--      <h2 class="subtitle">
+--        Hero subtitle
+--      </h2>
+--    </div>
+--  </div>
+--</section>
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ section [ class "hero", class "is-primary" ]
+            [ div [ class "hero-body" ]
+                [ div [ class "container" ]
+                    [ h1 [ class "title" ]
+                        [ text "HSCalendar"
+                        ]
+                    ]
+                ]
+            ]
+        , section [ class "section" ]
+            [ div [ class "container" ]
+                [ nav [ class "level" ]
+                    [ div [ class "level-left" ]
+                        [ div [ class "level-item" ]
+                            [ button [ class "button", onClick (AdjustPosix (previousDay model)) ] 
+                                [ text "Prev" ]
+                            ]
+                        ]
+                    , div [ class "level-item" ]
+                        [ p []
+                            [ text ("Posix time is " ++ posixToString model.zone model.posix) ]
+                        ]
+                    , div [ class "level-right" ]
+                        [ div [ class "level-item" ]
+                            [ button [ class "button", onClick (AdjustPosix (nextDay model)) ] 
+                                [ text "Next" ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
         ]
