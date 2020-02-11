a7aad62338982d2ab29aca132c89c9aa1c408cb8
jecaro <jeancharles.quillet@gmail.com>
2020-02-11 18:20:47 +0100

    Add fromString function

--- frontend/src/Api/IdleDayType/Extended.elm
+++ frontend/src/Api/IdleDayType/Extended.elm
@@ -1,4 +1,4 @@
-module Api.IdleDayType.Extended exposing (toString)
+module Api.IdleDayType.Extended exposing (fromString, toString)
 
 import Api exposing (IdleDayType(..))
 
@@ -12,3 +12,15 @@ toString dayType =
        UnpaidLeave -> "Unpaid leave"
        PublicHoliday -> "Public holiday"
        PartTime -> "Part time"
+
+fromString : String -> Result String IdleDayType
+fromString str = 
+    case str of
+       "Paid leave" -> Ok PaidLeave
+       "Family event" -> Ok FamilyEvent
+       "RTTE" -> Ok RTTE
+       "RTTS" -> Ok RTTS
+       "Unpaid leave" -> Ok UnpaidLeave
+       "Public holiday" -> Ok PublicHoliday
+       "Part time" -> Ok PartTime
+       _ -> Err "Bad string"    
