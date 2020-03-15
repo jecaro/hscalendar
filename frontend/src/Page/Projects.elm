6164cb1229fb2828c89531e07a74323a62a79462
jecaro <jeancharles.quillet@gmail.com>
2020-03-15 16:40:19 +0100

    Initialize the view for the project page

--- frontend/src/Page/Projects.elm
+++ frontend/src/Page/Projects.elm
@@ -1,13 +1,46 @@
 module Page.Projects exposing (view)
 
 import Browser exposing (Document)
-import Html exposing (div, header, li, text, ul)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, header, i, input, span, text)
+import Html.Attributes exposing (class, placeholder, readonly, value)
 import List exposing (map)
 
 import Api exposing (Project)
 
 import NavBar as NB exposing (view)
+
+viewProject : Project -> Html msg
+viewProject project = 
+    div [ class "field", class "is-grouped" ]
+        [ div [ class "control", class "is-expanded" ]
+            [ input 
+                [ class "input"
+                , class "is-static"
+                , value project.unProject
+                , readonly True 
+                ] []
+            ]
+        , div [ class "control" ]
+            [ button [ class "button", class "is-danger",class "is-outlined" ]
+                [ span [ class "icon" ] 
+                    [ i [ class "fas fa-times" ] [] ]
+                ]
+            ]
+        ]
+
+viewNewProject : Html msg
+viewNewProject = 
+    div [ class "field", class "is-grouped" ]
+        [ div [ class "control", class "is-expanded" ]
+            [ input [ class "input", placeholder "Add new project" ] []
+            ]
+        , div [ class "control" ]
+            [ button [ class "button", class "is-success", class "is-outlined" ]
+                [ span [ class "icon" ] 
+                    [ i [ class "fas fa-plus" ] [] ]
+                ]
+            ]
+        ]
 
 view : List Project -> Document msg
 view projects = 
@@ -25,8 +58,8 @@ view projects =
                             ]
                         , div [ class "card-content" ]
                             [ div [ class "content" ] 
-                                [ ul [] (map (\p -> li [] [ text p.unProject ]) projects)
-                                ]
+                                <| map viewProject projects 
+                                ++ [ viewNewProject ]
                             ]
                         ]
                     ]
