692bf96967b0041ff74633b4e0cc238cdf9e6eee
jecaro <jeancharles.quillet@gmail.com>
2026-07-02 19:22:23 +0200

    Fix the tests

--- flake.nix
+++ flake.nix
@@ -9,11 +9,7 @@
           haskell-to-elm = hlib.doJailbreak (hlib.unmarkBroken hprev.haskell-to-elm);
         };
       };
-      # Tests are disabled in the package build: 4 property tests fail at
-      # runtime against the newer persistent-sqlite / time libraries (sub-second
-      # TimeOfDay round-tripping and test DB isolation). Run them in the dev
-      # shell with `cabal test`.
-      hscalendar = hlib.dontCheck (haskellPackages.callCabal2nix "hscalendar" ./. { });
+      hscalendar = haskellPackages.callCabal2nix "hscalendar" ./. { };
     in
     {
       packages.x86_64-linux.default = hscalendar;
