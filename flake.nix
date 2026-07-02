c1a4ebc110200be45562122ddb75a909a550c64e
jecaro <jeancharles.quillet@gmail.com>
2026-07-02 22:43:12 +0200

    nix build the frontend and the whole app

--- flake.nix
+++ flake.nix
@@ -9,19 +9,58 @@
           haskell-to-elm = hlib.doJailbreak (hlib.unmarkBroken hprev.haskell-to-elm);
         };
       };
-      hscalendar = haskellPackages.callCabal2nix "hscalendar" ./. { };
+
+      # The backend in Haskell
+      backend = haskellPackages.callCabal2nix "hscalendar" ./. { };
+
+      # The Elm frontend.
+      # Elm dependencies are pinned with elm2nix (elm-srcs.nix + registry.dat);
+      # regenerate them with:
+      #   cd frontend && elm2nix convert > elm-srcs.nix && elm2nix snapshot
+      frontend = pkgs.stdenv.mkDerivation {
+        pname = "hscalendar-frontend";
+        version = "0.1.0.0";
+        src = ./frontend;
+        nativeBuildInputs = [ pkgs.elmPackages.elm ];
+        buildPhase =
+          (pkgs.elmPackages.fetchElmDeps {
+            elmPackages = import ./frontend/elm-srcs.nix;
+            elmVersion = "0.19.1";
+            registryDat = ./frontend/registry.dat;
+          })
+          + ''
+            ${backend}/bin/elm-generator -o src
+            elm make src/Main.elm --optimize --output=elm.js
+          '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r elm.js index.html assets $out/
+        '';
+      };
+
+      hscalendar = pkgs.runCommand "hscalendar-0.1.0.0" { } ''
+        mkdir -p $out
+        ln -s ${backend}/bin/* $out/
+        ln -s ${frontend} $out/frontend
+      '';
     in
     {
-      packages.x86_64-linux.default = hscalendar;
+      packages.x86_64-linux = {
+        inherit backend frontend hscalendar;
+        default = hscalendar;
+      };
 
       devShells.x86_64-linux.default =
         haskellPackages.shellFor {
-          packages = p: [ hscalendar ];
+          packages = p: [ backend ];
           withHoogle = true;
           buildInputs = [
-            haskellPackages.haskell-language-server
-            haskellPackages.ghcid
             haskellPackages.cabal-install
+            haskellPackages.ghcid
+            haskellPackages.haskell-language-server
+            pkgs.elm2nix
+            pkgs.elmPackages.elm
+            pkgs.elmPackages.elm-format
           ];
         };
     };
---
+++ frontend/elm-srcs.nix
@@ -0,0 +1,82 @@
+{
+
+      "NoRedInk/elm-json-decode-pipeline" = {
+        sha256 = "0y25xn0yx1q2xlg1yx1i0hg4xq1yxx6yfa99g272z8162si75hnl";
+        version = "1.0.0";
+      };
+
+      "elm-community/html-extra" = {
+        sha256 = "0n07a6dvk58ns237xh3law3zw2a9p2qcg5pmlmfwkpvafbngkf9x";
+        version = "3.3.0";
+      };
+
+      "elm-community/maybe-extra" = {
+        sha256 = "0vd8n8pxk16kzff3j92986cig0j0h5jdnb1yyf92i79sy30gn5vl";
+        version = "5.1.0";
+      };
+
+      "elm/browser" = {
+        sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+        version = "1.0.2";
+      };
+
+      "elm/core" = {
+        sha256 = "0gyk7lx3b6vx2jlfbxdsb4xffn0wdvg5yxldq50jr2kk5dzc2prj";
+        version = "1.0.4";
+      };
+
+      "elm/html" = {
+        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+        version = "1.0.0";
+      };
+
+      "elm/http" = {
+        sha256 = "008bs76mnp48b4dw8qwjj4fyvzbxvlrl4xpa2qh1gg2kfwyw56v1";
+        version = "2.0.0";
+      };
+
+      "elm/json" = {
+        sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+        version = "1.1.3";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "elm/url" = {
+        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+        version = "1.0.0";
+      };
+
+      "justinmimbs/date" = {
+        sha256 = "0gl31dsbzryc29749wwi4jfnidsyicbplawmc8ar1vbxaa39ld0v";
+        version = "3.2.0";
+      };
+
+      "krisajenkins/remotedata" = {
+        sha256 = "0m5bk0qhsjv14vajqrkph386696pnhj5rn51kgma8lwyvvx9ihw1";
+        version = "6.0.1";
+      };
+
+      "elm/bytes" = {
+        sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
+        version = "1.0.8";
+      };
+
+      "elm/file" = {
+        sha256 = "1rljcb41dl97myidyjih2yliyzddkr2m7n74x7gg46rcw4jl0ny8";
+        version = "1.0.5";
+      };
+
+      "elm/parser" = {
+        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+        version = "1.1.0";
+      };
+
+      "elm/virtual-dom" = {
+        sha256 = "0q1v5gi4g336bzz1lgwpn5b1639lrn63d8y6k6pimcyismp2i1yg";
+        version = "1.0.2";
+      };
+}
